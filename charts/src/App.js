475b6ae8ccdd06a48325e4b29c059387569f0b15
kevinmbuicode <kevinmushan@gmail.com>
2022-07-30 15:15:53 +0300

    BarChart working successfully.

---
+++ charts/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import BarChart from './components/Charts/BarChart';
+
+function App() {
+  return (
+    <div className="App">
+      <BarChart/>
+    </div>
+  );
+}
+
+export default App;
